0082997321f945f6c7cac7bf7b98c565476f5057
Marius Müller <marius@bscnet.de>
2023-12-21 16:50:36 +0100

    Changed React back to 17

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
-import React from "react";
+//import React from "react";
 import "./index.css";
 import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
-//import ReactDOM from "react-dom";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
+import { StrictMode } from "react";
+//import ReactDOM from "react-dom/client";
 
+/*
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+*/
 
-/*
 ReactDOM.render(
   <StrictMode>
     <App />
   </StrictMode>,
   document.getElementById("root")
-);*/
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
